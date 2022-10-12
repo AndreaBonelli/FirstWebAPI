b5e3a8c0ac20ca2e819ceeb2929ca88d68b67496
Andrea Bonelli <bonelliandrea97@gmail.com>
2022-10-12 13:02:51 +0200

    created methods fillList and Add

--- FirstWebAPI/Controllers/StringController.cs
+++ FirstWebAPI/Controllers/StringController.cs
@@ -6,19 +6,28 @@ namespace FirstWebAPI.Controllers
     [Route("[controller]")] // grazie a [] prende da solo "WeatherForecast"
     public class StringController : ControllerBase
     {
+        List<string> list = new List<string>();
+
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public StringController()
+        [HttpPost("fill")]
+        public string FillList()
         {
+            foreach (var s in Summaries)
+                list.Add(s);
+            return "lista riempita";
         }
 
-        [HttpGet()]
-        public string GetString(string s)
+        [HttpPost("add/{s}")]
+        public string AddToList(string s)
         {
+            list.Add(s);
             return "Stringa inserita: " + s;
         }
+
+        
     }
 }
